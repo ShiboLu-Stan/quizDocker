05bc608f44732b7d82cd97a7e592ec060e272a99
stanluu <42020235+stanluu@users.noreply.github.com>
2023-05-16 12:42:59 -0400

    Update AdminController.java

--- PersonalProject1_quiz/src/main/java/com/personalproject1/quiz/controller/AdminController.java
+++ PersonalProject1_quiz/src/main/java/com/personalproject1/quiz/controller/AdminController.java
@@ -36,6 +36,7 @@ public class AdminController {
         this.feedbackService = feedbackService;
         this.contactService = contactService;
         int i = 0; //test jenkins
+        int j = 0; //test docker
     }
 
 
